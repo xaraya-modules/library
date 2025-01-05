18dae75b054e4aa2b7abcbb2d5c2e3015b4e4c26
mikespub <github@mikespub.net>
2025-01-05 21:17:55 +0100

    remove context param from itemlinks trait

--- xaruserapi.php
+++ xaruserapi.php
@@ -18,7 +18,7 @@ function library_userapi_getitemtypes(array $args = [], $context = null)
 {
     $userapi = xarMod::getAPI('library');
     $userapi->setContext($context);
-    return $userapi->getItemTypes($args, $context);
+    return $userapi->getItemTypes($args);
 }
 
 /**
@@ -33,5 +33,5 @@ function library_userapi_getitemlinks(array $args = [], $context = null)
 {
     $userapi = xarMod::getAPI('library');
     $userapi->setContext($context);
-    return $userapi->getItemLinks($args, $context);
+    return $userapi->getItemLinks($args);
 }
